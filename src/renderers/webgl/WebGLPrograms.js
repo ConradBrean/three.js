b79e159926d6ac666dab4fd606d3e40238b610ef
Fil <dmitrieff773@gmail.com>
2018-02-12 23:20:39 +0300

    remove shadowcasters availability check for shader parameters

--- src/renderers/webgl/WebGLPrograms.js
+++ src/renderers/webgl/WebGLPrograms.js
@@ -189,7 +189,7 @@ function WebGLPrograms( renderer, extensions, capabilities ) {
 
 			dithering: material.dithering,
 
-			shadowMapEnabled: renderer.shadowMap.enabled && object.receiveShadow && shadows.length > 0,
+			shadowMapEnabled: renderer.shadowMap.enabled && object.receiveShadow /*&& shadows.length > 0*/,
 			shadowMapType: renderer.shadowMap.type,
 			shadowMapPacking: renderer.shadowMap.depthPacking,
 
